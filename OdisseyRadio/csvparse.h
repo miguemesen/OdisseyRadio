ab9d71e613a1a9e2cad8b7a06ac317952c6c708e
gabo1305 <gabrielsolcor@gmail.com>
2020-11-14 09:29:21 -0600

    Final Changes

--- OdisseyRadio/csvparse.h
+++ OdisseyRadio/csvparse.h
@@ -1,2 +1,34 @@
 
+#pragma once
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <string>
 
+class csvparse
+{
+public:
+
+    csvparse(const csvparse&) = delete;
+
+    static csvparse& Get()
+    {
+        static csvparse instance;
+        return instance;
+    }
+
+    static std::vector<std::vector<std::string>> myReadCsv(){return Get().Internal();}
+
+private:
+
+    std::vector<std::vector<std::string>> Internal()
+    {
+        std::ifstream ip("/home/migue/Desktop/fma_metadata/raw_tracks1.csv");
+        return readCSV(ip);
+    }
+
+    std::vector<std::vector<std::string>> readCSV(std::istream &in);
+    std::vector<std::string> readCSVRow(const std::string &row);
+    csvparse();
+};
